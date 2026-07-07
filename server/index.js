6e5aad296e1e13bbb7babc6d4d28cbe82d97c99c
Akshat Jain <jain.akshat.0730@gmail.com>
2026-07-07 22:35:47 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -4,10 +4,27 @@ const Y = require('yjs')
 const encoding = require('lib0/encoding')
 const decoding = require('lib0/decoding')
 const syncProtocol = require('y-protocols/sync')
+const awarenessProtocol = require('y-protocols/awareness')
 
 const messageSync = 0
+const messageAwareness = 1
+const messageQueryAwareness = 3
 
 const rooms = new Map()
+
+function getAwarenessClientIDs(update) {
+  const decoder = decoding.createDecoder(update)
+  const clientIDs = []
+  const len = decoding.readVarUint(decoder)
+
+  for (let i = 0; i < len; i++) {
+    clientIDs.push(decoding.readVarUint(decoder))
+    decoding.readVarUint(decoder)
+    decoding.readVarString(decoder)
+  }
+
+  return clientIDs
+}
 
 function getRoom(roomname) {
   let room = rooms.get(roomname)
@@ -15,13 +32,33 @@ function getRoom(roomname) {
 
   const doc = new Y.Doc()
   const clients = new Set()
+  const awareness = new awarenessProtocol.Awareness(doc)
 
-  room = { doc, clients }
+  room = { doc, clients, awareness }
 
   doc.on('update', (update, origin) => {
     const encoder = encoding.createEncoder()
     encoding.writeVarUint(encoder, messageSync)
     syncProtocol.writeUpdate(encoder, update)
+    const message = encoding.toUint8Array(encoder)
+
+    for (const client of clients) {
+      if (client !== origin && client.readyState === WebSocket.OPEN) {
+        client.send(message)
+      }
+    }
+  })
+
+  awareness.on('update', ({ added, updated, removed }, origin) => {
+    const changedClients = added.concat(updated, removed)
+    if (changedClients.length === 0) return
+
+    const encoder = encoding.createEncoder()
+    encoding.writeVarUint(encoder, messageAwareness)
+    encoding.writeVarUint8Array(
+      encoder,
+      awarenessProtocol.encodeAwarenessUpdate(awareness, changedClients)
+    )
     const message = encoding.toUint8Array(encoder)
 
     for (const client of clients) {
@@ -49,6 +86,18 @@ wss.on('connection', (ws, req) => {
   const room = getRoom(roomname)
 
   room.clients.add(ws)
+  ws._awarenessClientIDs = new Set()
+
+  const awarenessEncoder = encoding.createEncoder()
+  encoding.writeVarUint(awarenessEncoder, messageAwareness)
+  encoding.writeVarUint8Array(
+    awarenessEncoder,
+    awarenessProtocol.encodeAwarenessUpdate(
+      room.awareness,
+      Array.from(room.awareness.getStates().keys())
+    )
+  )
+  ws.send(encoding.toUint8Array(awarenessEncoder))
 
   ws.on('message', (data) => {
     const decoder = decoding.createDecoder(new Uint8Array(data))
@@ -61,11 +110,32 @@ wss.on('connection', (ws, req) => {
       if (encoding.length(encoder) > 1) {
         ws.send(encoding.toUint8Array(encoder))
       }
+    } else if (messageType === messageAwareness) {
+      const update = decoding.readVarUint8Array(decoder)
+      getAwarenessClientIDs(update).forEach((clientID) => ws._awarenessClientIDs.add(clientID))
+      awarenessProtocol.applyAwarenessUpdate(room.awareness, update, ws)
+    } else if (messageType === messageQueryAwareness) {
+      encoding.writeVarUint(encoder, messageAwareness)
+      encoding.writeVarUint8Array(
+        encoder,
+        awarenessProtocol.encodeAwarenessUpdate(
+          room.awareness,
+          Array.from(room.awareness.getStates().keys())
+        )
+      )
+      ws.send(encoding.toUint8Array(encoder))
     }
   })
 
   ws.on('close', () => {
     room.clients.delete(ws)
+    if (ws._awarenessClientIDs && ws._awarenessClientIDs.size > 0) {
+      awarenessProtocol.removeAwarenessStates(
+        room.awareness,
+        Array.from(ws._awarenessClientIDs),
+        ws
+      )
+    }
   })
 })
 
